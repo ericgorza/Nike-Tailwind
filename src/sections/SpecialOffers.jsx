7bd53c36c8afd919f3c5f8463f334503268ff51b
Eric <ericgorza@gmail.com>
2024-03-11 15:42:27 -0700

    tailwind css done

---
+++ src/sections/SpecialOffers.jsx
@@ -0,0 +1,30 @@
+import { offer } from "../assets/images"
+import Button from "../components/Button"
+import {arrowRight} from "../assets/icons"
+
+const SpecialOffers = () => {
+  return (
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} alt="" width={773} height={687}className="object-contain w-full"/>
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
+         <span className="text-coral-red inline-block mt-3">Special</span> Offer
+        </h2>
+        <p className='mt-7 lg:max-w-lg info-text'>Ensuring premium comfort and style, our meticulously crafter footwear is designed to elevate you experience, providing you with unmatched quality, innovation, and a touch of elegance</p>
+        <p className='mt-4 lg:max-w-lg info-text'>Our dedication to detail and excellence ensures your satisfaction</p>
+        <div className="mt-11 flex flex-wrap gap-5">
+          <Button label="Shop Now" iconURL={arrowRight}/>
+          <Button label="View Details" 
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default SpecialOffers
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducs from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import SpecialOffers from './SpecialOffers'
+import Subscribe from './Subscribe'
+
+export {
+    Hero,
+    PopularProducs,
+    SuperQuality,
+    Services,
+    CustomerReviews,
+    Footer,
+    SpecialOffers,
+    Subscribe
+}
